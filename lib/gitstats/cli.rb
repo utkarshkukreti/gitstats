b280bb87a6cd8286926cc76e5ae37f03891e71e0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-15 23:24:34 +0530

    Add some sample stats output to the CLI for now.

--- lib/gitstats/cli.rb
+++ lib/gitstats/cli.rb
@@ -1,7 +1,11 @@
 module GitStats
   class CLI
     def initialize(argv)
-      puts "You sent #{argv}"
+      repo = Repository.new(argv.first || '.')
+      puts "Your repo:\n#{repo.inspect}"
+      puts "You have #{repo.commits.size} commits in your repository."
+      puts "Here's the oldest one:\n#{repo.commits.first.inspect}"
+      puts "and here's the latest one:\n#{repo.commits.last.inspect}"
     end
   end
 end
