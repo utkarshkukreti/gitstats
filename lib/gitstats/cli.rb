773466f72ac669427cfbc44e67f57d2eea9f4dbe
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-28 21:04:52 +0530

    Refactor CLI into multiple methods.

--- lib/gitstats/cli.rb
+++ lib/gitstats/cli.rb
@@ -1,41 +1,54 @@
 module GitStats
   class CLI
     def initialize(argv)
-      options = Slop.parse!(argv, :help => true) do
+      parse_options(argv)
+      render_templates
+    end
+
+    def parse_options(argv)
+      @options = Slop.parse!(argv, :help => true) do
         on :f, 'force', 'Force replace output files'
         on :o, 'output', 'Output directory (defaults to ./output)', true, :default => './output'
+        on :t, 'templates', 'Directory containing templates to render', true,
+          :default => File.expand_path('../templates', __FILE__)
       end
-      # create output dir if it doesn't exist
-      output_dir = File.expand_path(options[:output])
+      @repository = Repository.new argv.first || '.'
+    end
+
+    def render_templates
+      output_dir = File.expand_path(@options[:output])
       unless File.directory?(output_dir)
         puts "#{output_dir} doesn't exist. Creating."
         FileUtils.mkdir_p(output_dir)
       end
-      repo = Repository.new(argv.first || '.')
-      locals = { commits: repo.commits }
-      skipped_files = 0
 
-      Dir[File.expand_path('../templates/*.haml', __FILE__)].each do |template|
-        rendered = Haml::Engine.new(File.read(template)).render(Object.new, locals)
-        output_file = File.join(output_dir, File.basename(template).gsub(/haml$/, 'html'))
-        if File.exist?(output_file)
-          print "#{output_file} exists. "
-          if options.force?
-            puts "Overwriting."
-            File.open(output_file, 'w') { |f| f << rendered }
-          else
-            puts "Skipping."
-            skipped_files += 1
-          end
+      templates = Dir[File.join(File.expand_path(@options[:templates]), '*.haml')]
+      written = templates.count do |template|
+        output = File.join(output_dir, "#{File.basename(template, 'haml')}html")
+        render_template(template, output)
+      end
+
+      if written < templates.size
+        puts "#{templates.size - written} file(s) were skipped. Call with '-f' to force overwrite."
+      end
+    end
+
+    def render_template(input, output)
+      if File.exist?(output)
+        print "#{output} exists. "
+        if @options.force?
+          puts "Overwriting."
         else
-          puts "Writing to #{output_file}"
-          File.open(output_file, 'w') { |f| f << rendered }
+          puts "Skipping."
+          return false
         end
+      else
+        puts "Writing to #{output}"
       end
 
-      if skipped_files > 0
-        puts "#{skipped_files} files were skipped. Call with '-f' to force overwrite."
-      end
+      locals = { commits: @repository.commits }
+      rendered = Haml::Engine.new(File.read(input)).render(Object.new, locals)
+      File.open(output, 'w') { |f| f << rendered }
     end
   end
 end
